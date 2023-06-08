7988c9d8d2790a85fc810053acae8ff5e43339e3
joaobrc <56743873+joaobrc@users.noreply.github.com>
2023-06-08 18:27:44 -0300

    criando teste tonica invalida e tonalidade invalida ou inexistente

--- tests/test_escalas.py
+++ tests/test_escalas.py
@@ -3,10 +3,12 @@ AAA
 arange, act - assets
 Arrumar - Agir  Garantir
 """
-from notas_musicais.escalas import escala
+from pytest import raises
+
+from notas_musicais.escalas import ESCALAS, NOTAS, escala
 
 
-def test_escala_deve_funcionar_com_notas_minuscolas():
+def test_deve_funcionar_com_notas_minuscolas():
 
     # arrumar
     tonica = 'c'
@@ -15,3 +17,26 @@ def test_escala_deve_funcionar_com_notas_minuscolas():
     result = escala(tonica, tonalidade)
     # Garantir
     assert result
+
+
+def test_deve_retornar_um_erro_quando_for_inserido_uma_tonica_que_nao_existe_e_retorna_as_notas_disponiveis():
+    tonica = 'K'
+    tonalidade = 'maior'
+    menssagem_de_erro = 'Nota inexistente tente uma dessas {}'.format(NOTAS)
+
+    with raises(ValueError) as error:
+        escala(tonica, tonalidade)
+    assert menssagem_de_erro == error.value.args[0]
+
+
+def test_deve_retornar_um_erro_quando_for_inserido_uma_tonalidade_inexistente_e_retornar_as_tonalidades():
+    tonica = 'A'
+    tonalidade = 'tonalidade'
+    menssagem__de_erro = (
+        'Tonalidade Inexisten ou nao disponive, '
+        'tente uma destas {}'.format(list(ESCALAS.keys()))
+    )
+
+    with raises(KeyError) as error:
+        escala(tonica, tonalidade)
+    assert menssagem__de_erro == error.value.args[0]
