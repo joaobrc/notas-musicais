9bbe263fcf6619c6e11accb57d61f104b6c9192d
joaobrc <56743873+joaobrc@users.noreply.github.com>
2023-06-13 21:15:47 -0300

    adicionando argumentos no arquivo cli

--- notas_musicais/cli.py
+++ notas_musicais/cli.py
@@ -2,13 +2,19 @@ from escalas import escala
 from rich.console import Console
 from rich.table import Table
 from typer import Argument, run
+from typing_extensions import Annotated
 
 console = Console()
-table = Table()
 
 
-def escalas():
-    notas, graus = escala('C', 'maior').values()
+def escalas(
+    tonica: Annotated[str, Argument(help='tonica da escala')] = 'C',
+    tonalidade: Annotated[
+        str, Argument(help='Tonalidade da escala')
+    ] = 'maior',
+):
+    table = Table()
+    notas, graus = escala(tonica, tonalidade).values()
     for grau in graus:
         table.add_column(grau)
     table.add_row(*notas)
